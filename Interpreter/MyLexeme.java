1cb54e71e7c591adc881eaf1b0116cba1dcdbf40
spm-pottery1 <155481649+spm-pottery1@users.noreply.github.com>
2025-10-07 17:36:04 -0400

    Initial Project + First Draft
    IDK Bro the prof said it was wrong :(

---
+++ Interpreter/MyLexeme.java
@@ -0,0 +1,34 @@
+
+public class MyLexeme {
+
+    /* requirements:
+     * - type needs to be able to differentiate between
+     * - NUM,ID,KEYWORD,OPERATOR,DELIMITER
+     */
+
+    public String type;
+    public Tuple sPos;
+ 
+
+    public MyLexeme(String type, int lineNum, int charNum){
+        this.type = type;
+        this.sPos = new Tuple(lineNum, charNum);
+    }
+    public MyLexeme(String type, Tuple tuple){
+        this.type = type;
+        this.sPos = tuple;
+    }
+    public MyLexeme(){
+        this.type = null;
+        this.sPos = null;
+    }
+    public String getKind() {
+        return this.type;
+    }
+    public Tuple get_sPos() {
+        return this.sPos;
+    }
+    public String toString() {
+        return "Type: " + this.type + ", Start Position: " + this.sPos.toString();
+    }       
+}
---
+++ Interpreter/Tuple.java
@@ -0,0 +1,41 @@
+public class Tuple{
+
+    /*
+    * - Requirements:
+    * - Create an object that holds two integers
+    */
+
+
+    public int x;
+    public int y;
+
+    public Tuple(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public Tuple(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+    public void setX(int i){
+        this.x = i;
+    }
+    public void setY(int i){
+        this.y = i;
+    }
+    public void set(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public String toString(){
+        return "("+this.x+","+this.y+")";
+    }
+    
+}
